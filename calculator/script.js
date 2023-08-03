5efd924615f4255bf33da570b84d7042b55974c1
Cornelius Soon <cornelius.soon@gmail.com>
2023-08-04 10:30:20 +1200

    Add calculator project

---
+++ calculator/script.js
@@ -0,0 +1,213 @@
+// html number buttons
+// 0 through to 9
+// data-number
+// html clear button
+// data-reset
+// html operator buttons
+// data-operator
+// html hint display zone
+// html working display zone
+// html equal sign
+// data-equal
+// html dot sign
+// data-dot
+
+// store inputs
+// logic to run inputs
+// event listener for number button press
+// event listener reset
+// event listener for operator
+// event listener for equal
+// event listener for dot
+// "basic 1 + 2 = 3" operations
+// on click 1
+// store operand 1
+// show operand 1 on working display zone
+// on click +
+// store operator
+// show operand 1 and operator in hint display zone
+// on click 2
+// store operand 2
+// show operand 1, operator and operand 2 in hint display zone
+// show operand 2 on working display zone.
+// on click =
+// process stored operand and operators
+// show output on working display zone.
+// clear working memory and reset calculator
+// bonus: backspace button
+// bonus: handling for order of operations * and divide?
+// bonus: handle decimals
+// bonus: handling for brackets
+// TODO equal sign needs to repeat the action previous
+// TODO equal sign with one operandUnit needs to show operandUnit and then clear it
+
+
+let workingMemory = [];
+let operandUnit = "";
+const operatorInstructions = ['add', 'subtract', 'multiply', 'divide'];
+
+const operands = document.querySelectorAll('button[data-number]');
+const operators = document.querySelectorAll('button[data-operator]');
+const workingDisplay = document.querySelector('.working-display');
+const hintDisplay = document.querySelector('.hint-display');
+const resetButton = document.querySelector('button[data-utility]');
+
+function getOperandInput(event) {
+    return event.target.getAttribute('data-number');
+}
+
+function storeOperandAsUnit(input) {
+    operandUnit += input;
+}
+
+function updateWorkingDisplay() {
+    workingDisplay.textContent = operandUnit;
+}
+
+function clearWorkingDisplay() {
+    workingDisplay.textContent = "";
+}
+
+resetButton.addEventListener('click', () => {
+    clearOperandUnit();
+    clearWorkingMemory();
+    clearWorkingDisplay();
+})
+
+operands.forEach(node => {
+    node.addEventListener('click', event => {
+        let input = getOperandInput(event);
+        storeOperandAsUnit(input);
+        updateWorkingDisplay();
+        console.log('operandUnit');
+        console.log(operandUnit);
+        console.log('workingMemory');
+        console.log(workingMemory);
+    })
+})
+
+operators.forEach(node => {
+    node.addEventListener('click', event => {
+        let input = event.target.getAttribute('data-operator');
+
+        if (input === 'equal' && (workingMemory.length === 0 || workingMemory.length === 1)) {
+            console.log("CHECK ONE")
+            return;
+        }
+
+        if (workingMemory.length === 0 && operandUnit.length === 0) {
+            console.log("CHECK TWO")
+            return;
+
+        } // prevents workingMemory being used when there are no operators
+          // but there are operands being called
+
+        if (operandUnit.length > 0) {
+            workingMemory.push(+operandUnit);
+        }
+        console.log('operandUnit');
+        console.log(operandUnit);
+
+        // TODO separate logic out here into different functions
+        // Then set up another branch to handle the condition where "equals" is being pressed
+        // multiple times in relation to something like 1 + 2 =
+        // pressing = again should return 5, since the previous result was 3 and you add 2.
+
+        if (workingMemory.length === 3) {
+            let result = calculate(workingMemory);
+            workingMemory = []; // reset working memory;
+            workingMemory.push(result);
+            console.log('workingMemory')
+            console.log(workingMemory);
+        }
+
+        clearOperandUnit();
+        clearWorkingDisplay();
+        switch(input) {
+            case('add'):
+                checkOperatorDuplicateExists();
+                workingMemory.push('add');
+                break
+            case('subtract'):
+                checkOperatorDuplicateExists();
+                workingMemory.push('subtract');
+                break
+            case('multiply'):
+                checkOperatorDuplicateExists();
+                workingMemory.push('multiply');
+                break
+            case('divide'):
+                checkOperatorDuplicateExists();
+                workingMemory.push('divide');
+                break
+            default:
+                console.log("ERROR: No operators found.")
+                break
+        }
+        console.log("workingMemory:")
+        console.log(workingMemory);
+    })
+})
+
+function checkOperatorDuplicateExists() {
+    if (workingMemory.some((element) => operatorInstructions.includes(element))) {
+        workingMemory.pop();
+    }
+}
+
+function clearOperandUnit() {
+    operandUnit = "";
+}
+
+function clearWorkingMemory() {
+    workingMemory = [];
+}
+function multiply(array) {
+    return array.reduce((total, value) => {return total * value}, 1)
+}
+
+function add(array) {
+    return array.reduce((total, value) => {return total + value}, 0)
+}
+
+function subtract(array) {
+    return array.reduce((total, value) => {return total - value}, )
+}
+
+function divide(array) {
+    return array.reduce((total, value) => {return total / value}, )
+}
+
+function calculate(array) {
+    if (array.includes('add')) {
+        let index = array.indexOf('add');
+        array.splice(index, 1);
+        return add(array);
+    }
+
+    if (array.includes('subtract')) {
+        let index=array.indexOf('subtract');
+        array.splice(index, 1);
+        return subtract(array)
+    }
+
+    if (array.includes('multiply')) {
+        let index = array.indexOf('multiply');
+        array.splice(index, 1);
+        return multiply(array);
+    }
+
+    if (array.includes('divide')) {
+        let index = array.indexOf('divide');
+        array.splice(index, 1);
+        return divide(array);
+    }
+
+    return 1;
+}
+
+
+function startApp() {
+
+}
+
