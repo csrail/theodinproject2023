92ded46ec924afb62911c80cb49c9879bfd98132
Cornelius Soon <cornelius.soon@gmail.com>
2023-08-15 12:17:03 +1200

    Add features: drawBook, renderLibrary, add new book, delete book

--- library/script.js
+++ library/script.js
@@ -1,18 +1,59 @@
+let shelf = document.querySelector('.shelf')
+
+let bookCounter = 0;
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, isRead) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = Boolean(read)
+    this.isRead = Boolean(isRead)
+    this.bookIndex = bookCounter
+    bookCounter += 1;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
-function drawLibrary() {
+function drawBook(book) {
+    const bookObject = document.createElement('div');
+    const bookAttribution = document.createElement('div');
+    const bookMetaData = document.createElement('div');
+    const bookTitle = document.createElement('div');
+    const bookAuthor = document.createElement('div');
+    const bookPages = document.createElement('div');
+    const bookIsRead = document.createElement('div');
+    const bookDataIndex = document.createElement('div');
 
+    bookObject.classList.add('book');
+    bookAttribution.classList.add('attribution');
+    bookMetaData.classList.add('metadata');
+
+    bookDataIndex.setAttribute('data-index', book.bookIndex);
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = `${book.pages} pages`;
+    bookDataIndex.textContent = book.bookIndex;
+
+    book.isRead === true ? bookIsRead.textContent = "Read" : bookIsRead.textContent = "Unread"
+
+    bookAttribution.appendChild(bookTitle);
+    bookAttribution.appendChild(bookAuthor);
+    bookMetaData.appendChild(bookPages);
+    bookMetaData.appendChild(bookIsRead);
+    bookObject.appendChild(bookAttribution);
+    bookObject.appendChild(bookMetaData);
+    bookObject.appendChild(bookDataIndex);
+
+    shelf.appendChild(bookObject);
+}
+
+function drawLibrary() {
+    for (const book of myLibrary) {
+        drawBook(book);
+    }
 }
 
 let book1 = new Book("Don Quixote", "Miguel De Cervantes", "952", false);
@@ -25,6 +66,31 @@ addBookToLibrary(book2)
 addBookToLibrary(book3)
 addBookToLibrary(book4)
 
+drawLibrary();
 
+const formAddBookButton = document.querySelector('#form-add-book-button');
+const formInputBookTitle = document.querySelector('#input-book-title');
+const formInputBookAuthors = document.querySelector('#input-book-authors');
+const formInputBookPages = document.querySelector('#input-book-title');
+const formInputBookIsRead = document.querySelector('#input-book-read');
 
-console.log(myLibrary);
+formAddBookButton.addEventListener('click', (event) => {
+    const bookObject = new Book(
+        formInputBookTitle.value,
+        formInputBookAuthors.value,
+        formInputBookPages.value,
+        formInputBookIsRead.value)
+    drawBook(bookObject);
+})
+// formInputBookTitle.value
+const formRemoveBookButton = document.querySelector('#form-remove-book-button');
+const formInputBookIndex = document.querySelector('#input-remove-book');
+
+formRemoveBookButton.addEventListener('click', () => {
+    let bookIndex = +formInputBookIndex.value;
+    let bookObject = myLibrary.find((obj) => obj.bookIndex === bookIndex);
+    let bookArrayIndex = myLibrary.findIndex((obj) => obj === bookObject);
+    let book = document.querySelector(`[data-index='${bookIndex}']`).parentNode;
+    myLibrary.splice(bookArrayIndex, 1);
+    book.remove();
+})
