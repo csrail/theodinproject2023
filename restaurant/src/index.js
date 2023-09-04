cfd809d23b029c1322fb6cea0ba76386edd23513
Cornelius Soon <cornelius.soon@gmail.com>
2023-09-05 11:54:30 +1200

    Abstract data into separate json object

--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -15,6 +15,13 @@
 // displayController object
 // TODO make Order factory function shopping cart
 
+// Content should be responsible for building the content
+// Navigation should be responsible for getting the content
+// Therefore move build logic from Navigation and place into Content
+// Therefore add get logic into Content so Navigation can request it
+// Need to write display methods
+const consumableFood = require ('./food.json');
+
 const Content = () => {
     const { buildMenu, getContainer } = MenuController();
 
@@ -34,25 +41,13 @@ const Content = () => {
         return contentElement;
     }
 
-    const getMenuContent = () => {
-        return menuContent
-    }
-
-    const setMenuContent = () => {
-        return buildMenu();
-    }
-
-    return { getContentElement, createNewContentElement, getMenuContent, setMenuContent, aboutContent, contactContent,
-        getContainer,
-        buildMenu, }
+    return { getContentElement, createNewContentElement, aboutContent, contactContent, getContainer, buildMenu, }
 }
 
 const Navigation = () => {
     const {
         getContentElement,
         createNewContentElement,
-        getMenuContent,
-        setMenuContent,
         buildMenu,
         getContainer,
         aboutContent,
@@ -92,7 +87,6 @@ const Navigation = () => {
     }
 
     return { initialiseLandingPage, initialiseNavigation }
-
 }
 
 const MenuController = () => {
@@ -144,15 +138,11 @@ const Menu = () => {
     const foodCollection = [];
     const beverageCollection = [];
 
-    const consumable = Consumable({ name: "Laksa Soup", description: "Coconut-based spicy soup", price: "20.80", course: "Main"});
-    const consumable0 = Consumable({name: "Chicken Wing", description: "Deep fried specially made chicken wing", price: "9.80", course: "Starter"});
-    const consumable1 = Consumable({name: "Mini Spring Roll", description: "Deep fried mini spring rolls, served with sweet chilli sauce", price: "10.80", course: "Starter"});
-    const consumable2 = Consumable({name: "Malay Tofu", description: "Crispy yum nom tofu with cucumber, carrots, sesame, sweet chilli sauce. Crispy outside and soft inside", price: "12.80", course: "Starter"});
     const collectConsumable = (consumable) => {
-        if (consumable.getType === "Food") {
+        if (consumable.getType() === "Food") {
             foodCollection.push(consumable)
             console.log("Added consumable to foodCollection")
-        } else if (consumable.getType === "Beverage") {
+        } else if (consumable.getType() === "Beverage") {
             beverageCollection.push(consumable)
             console.log("Added consumable to beverageCollection")
         } else {
@@ -160,10 +150,9 @@ const Menu = () => {
         }
     }
 
-    collectConsumable(consumable);
-    collectConsumable(consumable0);
-    collectConsumable(consumable1);
-    collectConsumable(consumable2);
+    consumableFood.forEach(food => {
+        collectConsumable(Consumable(food))
+    })
 
     const getFoodCollection = () => { return foodCollection }
     const getBeverageCollection = () => { return beverageCollection }
@@ -193,7 +182,7 @@ const Consumable = (obj = {}) => {
     }
 
     _setType(obj);
-    const getType = type;
+    const getType = () => { return type }
 
     return { getType, getName, getDescription, getPrice, getCourse, getStyle };
 }
@@ -226,8 +215,3 @@ const main = (() => {
 
     return {}
 })();
-
-// a menuController object can run the collectConsumable function belonging to the Menu object
-// how does the resolution work here?
-// inside the menuController function expression, Menu is invoked and assigned to const collectConsumable
-// so if I run menuController.collectConsumable, I return Menu()
