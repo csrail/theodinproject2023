af29b623717fd072bc646e0b6ee10a1b1fd72097
Cornelius Soon <cornelius.soon@gmail.com>
2023-09-05 13:10:30 +1200

    Rename functions to describe their output better, abstract content functions out of Navigation and into Content, use switch statements to evaluate state instead of multiple if statements

--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -23,12 +23,12 @@
 const consumableFood = require ('./food.json');
 
 const Content = () => {
-    const { buildMenu, getContainer } = MenuController();
+    const { buildMenu, getMenuElements } = MenuController();
 
-    let contentElement = document.querySelector('#content');
-    const menuContent = document.createElement('div');
+    let contentElement = document.createElement('div');
     const aboutContent = document.createElement('div');
     const contactContent = document.createElement('div');
+    contentElement.id = 'content'
     aboutContent.textContent = "about"
     contactContent.textContent = "contact"
 
@@ -41,7 +41,22 @@ const Content = () => {
         return contentElement;
     }
 
-    return { getContentElement, createNewContentElement, aboutContent, contactContent, getContainer, buildMenu, }
+    const displayMenuContent = () => {
+        getContentElement().appendChild(getMenuElements());
+        document.body.append(getContentElement());
+    }
+
+    const displayAboutContent = () => {
+        getContentElement().appendChild(aboutContent);
+        document.body.append(getContentElement());
+    }
+
+    const displayContactContent = () => {
+        getContentElement().appendChild(contactContent);
+        document.body.append(getContentElement())
+    }
+
+    return { getContentElement, createNewContentElement, displayAboutContent, displayContactContent, buildMenu, displayMenuContent }
 }
 
 const Navigation = () => {
@@ -49,14 +64,14 @@ const Navigation = () => {
         getContentElement,
         createNewContentElement,
         buildMenu,
-        getContainer,
-        aboutContent,
-        contactContent,
+        displayMenuContent,
+        displayAboutContent,
+        displayContactContent,
     } = Content();
 
     const initialiseLandingPage = () => {
         buildMenu();
-        getContentElement().appendChild(getContainer());
+        displayMenuContent();
     }
     const initialiseNavigation = () => {
         const navElement = document.querySelector('nav')
@@ -66,22 +81,20 @@ const Navigation = () => {
                 getContentElement().remove();
                 createNewContentElement();
 
-                if (item.getAttribute('data-navigation') === 'menu') {
-                    buildMenu();
-                    getContentElement().appendChild(getContainer())
-                    document.body.append(getContentElement());
+                switch (item.getAttribute('data-navigation')) {
+                    case 'menu':
+                        buildMenu();
+                        displayMenuContent();
+                        break
+                    case 'about':
+                        displayAboutContent();
+                        break
+                    case 'contact':
+                        displayContactContent();
+                        break
+                    default:
+                        break
                 }
-
-                if (item.getAttribute('data-navigation') === 'about') {
-                    getContentElement().appendChild(aboutContent);
-                    document.body.append(getContentElement());
-                }
-
-                if (item.getAttribute('data-navigation') === 'contact') {
-                    getContentElement().appendChild(contactContent);
-                    document.body.append(getContentElement())
-                }
-
             })
         })
     }
@@ -91,24 +104,24 @@ const Navigation = () => {
 
 const MenuController = () => {
     const { getFoodCollection } = Menu();
-    const { getContainer, makeContainer } = MenuView();
+    const { getMenuElements, buildMenuElements } = MenuView();
 
     const buildMenu = () => {
         getFoodCollection().forEach(item => {
-            makeContainer(item);
+            buildMenuElements(item);
         })
     }
 
-    return { getFoodCollection, makeContainer, getContainer, buildMenu }
+    return { getFoodCollection, buildMenuElements, getMenuElements, buildMenu }
 }
 
 const MenuView = () => {
     const containerElement = document.createDocumentFragment();
 
-    const getContainer = () => {
+    const getMenuElements = () => {
         return containerElement;
     }
-    const makeContainer = (obj) => {
+    const buildMenuElements = (obj) => {
         const content = document.createElement('div');
         content.classList.add('consumable');
 
@@ -131,7 +144,7 @@ const MenuView = () => {
         return containerElement.appendChild(content);
     }
 
-    return { getContainer, makeContainer }
+    return { getMenuElements, buildMenuElements }
 }
 
 const Menu = () => {
