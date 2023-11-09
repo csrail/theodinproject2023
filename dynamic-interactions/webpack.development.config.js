01725a24c1720523d1e293f9620b36ce3f134a78
Cornelius Soon <cornelius.soon@gmail.com>
2023-11-10 12:02:23 +1300

    Initialise project

---
+++ dynamic-interactions/webpack.development.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      watch: true,
+    },
+    liveReload: true,
+    compress: true,
+  },
+};
