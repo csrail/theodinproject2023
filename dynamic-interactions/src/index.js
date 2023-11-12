0d06ae57f0695d35eff328c50cc0441678153407
Cornelius Soon <cornelius.soon@gmail.com>
2023-11-13 10:25:51 +1300

    Setup image-loader, set up htmlwebpack plugin with templates, set up base javascript file

--- dynamic-interactions/src/index.js
+++ dynamic-interactions/src/index.js
@@ -1,4 +1,23 @@
 import "./style.css";
+import imageGlobe from "./images/globe.jpg";
+
+const globeImage = new Image(500, 500);
+globeImage.src = imageGlobe;
 
 const mainElement = document.querySelector("main");
-mainElement.classList.add("red");
+mainElement.appendChild(globeImage);
+
+const navElement = document.querySelector("nav");
+navElement.classList.add("red");
+navElement.addEventListener("click", () => {
+    console.log("test");
+    document.querySelectorAll("nav li").forEach((element) => {
+        element.classList.add("visible");
+    });
+});
+
+// window.addEventListener("click", () => {
+//     document.querySelectorAll(".visible").forEach((element) => {
+//         element.classList.remove("visible");
+//     });
+// });
--- dynamic-interactions/webpack.development.config.js
+++ dynamic-interactions/webpack.development.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -13,16 +14,28 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "src"),
       watch: true,
     },
     liveReload: true,
     compress: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
   // optimization: {
   //   runtimeChunk: "single",
   // },
