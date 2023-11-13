90ead26fa7082889c8cc5d305d9fe6bcc26f552a
Cornelius Soon <cornelius.soon@gmail.com>
2023-11-14 10:35:45 +1300

    Add source mapping

--- dynamic-interactions/webpack.development.config.js
+++ dynamic-interactions/webpack.development.config.js
@@ -2,41 +2,42 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "src"),
-      watch: true,
+    mode: "development",
+    entry: "./src/index.js",
+    devtool: "eval-source-map",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
-    liveReload: true,
-    compress: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
-      inject: "head",
-      scriptLoading: "defer",
-    }),
-  ],
-  // optimization: {
-  //   runtimeChunk: "single",
-  // },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "src"),
+            watch: true,
+        },
+        liveReload: true,
+        compress: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            filename: "index.html",
+            inject: "head",
+            scriptLoading: "defer",
+        }),
+    ],
+    // optimization: {
+    //   runtimeChunk: "single",
+    // },
 };
